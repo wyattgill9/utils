fe23d6e971e1167ed34e1ea8609106d8d4ec8c3a
Shidowy <101477459+Shidowy@users.noreply.github.com>
2025-03-11 14:30:40 -0700

    no more C code

--- clibs/example.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void hello_from_c() {
-    printf("Hello from C!\n");
-}
-
-int add(int a, int b) {
-    return a + b;
-}
--- clibs/example.h
+++
@@ -1,7 +0,0 @@
-#ifndef EXAMPLE_H
-#define EXAMPLE_H
-
-void hello_from_c();
-int add(int a, int b);
-
-#endif
